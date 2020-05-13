9e88862f864be46eea11f36b2f56c499e4d8814e
Michael Bade <mbade314@gmail.com>
2020-05-13 19:07:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:language]
+     string = programmer_hash[:dennis_ritchie][:language]
+     string.join
 end
 
 def adding_matz
